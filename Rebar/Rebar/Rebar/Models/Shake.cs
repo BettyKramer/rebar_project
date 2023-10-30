a400fbe4c2e4207bd1e1fe4a0489a0970cf5dcf5
bettyKramer <bettykr2003@gmail.com>
2023-10-30 21:11:30 +0200

    create shake class

--- Rebar/Rebar/Rebar/Models/Shake.cs
+++ Rebar/Rebar/Rebar/Models/Shake.cs
@@ -2,5 +2,11 @@
 {
     public class Shake
     {
+       Guid Id { get; set; }
+       public string Name { get; set; }
+       public string Description { get; set; }
+        public int PriceS { get;set; }
+        public int PriceM { get;set; }
+        public int PriceL { get;set;}
     }
 }
